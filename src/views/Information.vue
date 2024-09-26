c0ab549f21ee628c00ff3d731744d9365f84ee08
Pratik Labh <pratiklabh10@gmail.com>
2024-09-26 15:55:09 +0545

    Added feature for adding or removing a code

--- src/views/Information.vue
+++ src/views/Information.vue
@@ -1,60 +1,69 @@
 <template>
   <div class="info-form">
     <h2> Information Form </h2>
+    <Button icon="pi pi-plus" class="p-button-success" @click="addRow"/>
+
     <Form @submit="handleSubmit">
-      <DataTable :value="info">
+      <DataTable :value="info" :dataKey="'id'">
         <Column header="Name">
           <template #body="{ data }">
-            <Field name="name" rules="required" v-slot="{field}">
+            <Field :name="`name-${data.id}`" rules="required" v-slot="{field}">
               <InputText
-                  id="name"
+                  :id="`name-${data.id}`"
                   v-bind="field"
                   v-model="data.name"
               />
-              <ErrorMessage name="name" class="error"/>
+              <ErrorMessage :name="`name-${data.id}`" class="error"/>
             </Field>
           </template>
         </Column>
 
         <Column header="Phone">
-          <template #body="{data}">
-            <Field name="phone" rules="required" v-slot="{field}">
+          <template #body="{ data }">
+            <Field :name="`phone-${data.id}`" rules="required" v-slot="{field}">
               <InputText
-                  id="phone"
+                  :id="`phone-${data.id}`"
                   v-bind="field"
                   v-model="data.phone"
               />
-              <ErrorMessage name="phone" class="error"/>
+              <ErrorMessage :name="`phone-${data.id}`" class="error"/>
             </Field>
           </template>
         </Column>
 
         <Column header="Address">
           <template #body="{data}">
-            <Field name="address" rules="required" v-slot="{field}">
+            <Field :name="`address-${data.id}`" rules="required" v-slot="{field}">
               <InputText
-                  id="address"
+
+                  :id="`address-${data.id}`"
                   v-bind="field"
                   v-model="data.address"
               />
-              <ErrorMessage name="address" class="error"/>
+              <ErrorMessage :name="`address-${data.id}`" class="error"/>
             </Field>
           </template>
         </Column>
+        <Column header="Actions">
+          <template #body="{data}">
+            <Button icon="pi pi-trash" class="p-button-danger" @click="removeRow(data.id)"/>
+          </template>
+        </Column>
+
       </DataTable>
+
 
       <Button type="submit" class="submit-button">
         Submit
       </Button>
 
     </Form>
-  </div>
 
+
+  </div>
 </template>
 
-
 <script setup>
-
 import {ref} from "vue";
 import {defineRule, ErrorMessage, Field} from "vee-validate";
 import {required} from "@vee-validate/rules";
@@ -62,20 +71,31 @@ import {required} from "@vee-validate/rules";
 defineRule('required', required);
 
 const info = ref([
-  {name: '', phone: '', address: ''},
-])
+  {id: '1', name: '', phone: '', address: ''},
+]);
 
+const addRow = () => {
+  const newId = info.value.length + 1;
+  info.value.push({id: newId.toString(), name: '', phone: '', address: ''});
+}
+
+const removeRow = (id) => {
+  info.value = info.value.filter(row => row.id !== id);
+}
 
 const handleSubmit = () => {
-
-  alert('Form Submitted !!!')
-  console.log('submitted')
-}
+  alert('Form Submitted !!!');
+  console.log('submitted', info.value);
+};
 </script>
 
 <style scoped>
 .info-form {
   margin: 0 auto;
+}
+
+.p-button-success {
+  margin: 1rem;
 }
 
 .submit-button {
